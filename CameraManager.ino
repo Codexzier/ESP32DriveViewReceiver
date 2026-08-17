cf1e3557c635a3d3a0921518a3439be3c20ab83f
Codexzier <johannes.paul.langner@live.de>
2026-08-17 21:56:16 +0200

    add cam pin and cam manager

---
+++ CameraManager.ino
@@ -0,0 +1,55 @@
+void cameraInit() {
+  camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+
+    // data pins
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+
+    // control gpio
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+
+    //config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    //config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+
+    // not connected
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+
+    config.xclk_freq_hz = 20000000;
+
+    // Bitmap Einstellung
+    config.pixel_format = PIXFORMAT_RGB565;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+    config.frame_size = FRAMESIZE_240X240;
+    //#if CONFIG_IDF_TARGET_ESP32S3
+    //  config.fb_count = 2;
+    //#endif
+    //}
+
+    Serial.println("init camera");
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        delay(1000);
+        return;
+    }
+}
--- ESP32DriveViewReceiver.ino
+++ ESP32DriveViewReceiver.ino
@@ -19,24 +19,27 @@
 #include "esp_camera.h"
 #include "camera_pins.h"
 
-// ========================================================================================
-// Access Point and network server
+// ==================================================
+// WiFi
 #include <WiFi.h>
-#include <NetworkClient.h>
-#include <WiFiAP.h>
 
 const char *ssid = "fpv_remotecontroller";
 const char *password = "12345678";
 
-const char* serverIP = "192.168.4.2";  // IP des ESP32-Servers
-const uint16_t serverPort = 8080;         // Port des Servers
+// setup static ip adress
+IPAddress localIP(192, 168, 4, 2);      // target IP adress for the ESP32
+IPAddress gateway(192, 168, 4, 1);      // Router-IP, but it dosen't matter
+IPAddress subnet(255, 255, 255, 0);     // Subnetzmaske
+IPAddress dns(255, 255, 255, 0);        // base setup 
+
+const char* _serverIP = "192.168.4.1";  // IP des ESP32-Servers
+const uint16_t _serverPort = 8080;         // Port des Servers
 WiFiClient _client;
 
 long _timeoutForReconnect = 30;
 
 // ========================================================================================
 // any
-
 
 long _lastMillis = 0;
 
@@ -51,105 +54,89 @@ void setup() {
   // init camera
   cameraInit();
 
-  // ----------------------------------------------
-  // setup access point
-  if (!WiFi.softAP(ssid, password)) {
-    log_e("Soft AP creation failed.");
-    Serial.println("Soft AP creation failed.");
-    while (1);
+// ----------------------------------------------
+  // setup WLAN
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  //WiFi.config(localIP, gateway, subnet, dns); // Statische IP setzen
+  //WiFi.setAutoReconnect(true);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("WiFi Status: "); Serial.println(WiFi.status());
+    Serial.print("Hostname: "); Serial.println(WiFi.getHostname());
+    Serial.print("Auto Reconnect: "); Serial.println(WiFi.getAutoReconnect());
+
+    for(int i = 0; i < 40; i++) {
+      Serial.print(".");
+    }
+
+    Serial.println("");
+
+    delay(100);
   }
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  Serial.println("WiFi connected! ");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP()); 
 
-  // ----------------------------------------------
-  // start server
-  Serial.print("start server with port number ");
-  char buffer[12];
-  sprintf(buffer, "%d", _serverPort);
-  Serial.println(buffer);
+  connectToServer();
+  Serial.println("New Client.");  // print a message out the serial port
 
-  _server.begin(_serverPort); 
-
-  _lastMillis = millis();
-
-  Serial.println("Server started! ");
+    _lastMillis = millis();
 }
 
 void loop() {
-
   
   long actual = millis();
   if(actual < _lastMillis + 2000) {
      return;
    }
    _lastMillis = actual;
+   connectToServer();
 
-  Serial.println("------------------------------");
-  Serial.print("IP: "); Serial.println(WiFi.localIP());
-  Serial.println("wait for client connecting");
-  WiFiClient localclient = _server.accept();
-
-
-  if(localclient) {
+  if(_client) {
 
     Serial.println("connection accept");
 
-    while(localclient.connected()){
+    while(_client.connected()){
       Serial.println("connected ");
-
-      //digitalWrite(LED_BUILTIN, true);
 
       camera_fb_t *fb = esp_camera_fb_get();
       if(fb){
         Serial.println("");
       }
-      localclient.write(fb->buf, fb->len);
+      _client.write(fb->buf, fb->len);
       esp_camera_fb_return(fb);
-      //digitalWrite(LED_BUILTIN, false);
 
       delay(10);
     }
 
-    localclient.stop();
+    _client.stop();
     Serial.println("Connection break!");
 
     for(int i = 0; i < 4; i++) {
-      //digitalWrite(LED_BUILTIN, true);
-      delay(500);
-      //digitalWrite(LED_BUILTIN, false);
-      delay(500);
+      delay(1000);
       Serial.print("Countdown: "); Serial.println(i, DEC);
     }
     Serial.println();
   }
-  // else {
-  //   Serial.println("try in two second");
-  //   _server.stop();
-  //   delay(2000);
-  //   _server.setTimeout(3);
-  //   _server.begin(_serverPort);
-  // }
 
   Serial.println("try in two second");  
 }
 
-// void sendNextPicture() {
+void connectToServer() {
+  Serial.print("connect to server with port number ");
+  char buffer[12];
+  sprintf(buffer, "%d", _serverPort);
+  Serial.println(buffer);
 
-//   // long actual = millis();
-//   // if(actual < _lastMillis + 10) {
-//   //   return;
-//   // }
-//   // _lastMillis = actual;
+  while(!_client.connect(_serverIP, _serverPort)){
 
-//   digitalWrite(LED_BUILTIN, true);
+    Serial.println("No connection with a client!");
+    delay(1000);
+    Serial.print("Connecto to IP "); Serial.println(_serverIP);
+  }
 
-//   camera_fb_t *fb = esp_camera_fb_get();
-//   if(fb){
-//     Serial.println("");
-//   }
-//   _client.write(fb->buf, fb->len);
+Serial.print("IP: "); Serial.println(WiFi.localIP());
 
-//   digitalWrite(LED_BUILTIN, false);
-// }
+}
