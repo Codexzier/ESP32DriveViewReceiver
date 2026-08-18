2e2605a59b5db3fb8a918818adfd4a5993e73ed9
Codexzier <johannes.paul.langner@live.de>
2026-08-18 18:52:48 +0200

    it runs

--- ESP32DriveViewReceiver.ino
+++ ESP32DriveViewReceiver.ino
@@ -9,57 +9,50 @@
 // Stand:         17.08.2026
 // ========================================================================================
 
-#include <Arduino.h>
+// ESP32-S3 Sense with camera
 
 // ==================================================
 // Camera
-// TIP: need an enabled PSRAM
 #define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
 
 #include "esp_camera.h"
 #include "camera_pins.h"
 
-// ==================================================
-// WiFi
-#include <WiFi.h>
 
 const char *ssid = "fpv_remotecontroller";
 const char *password = "12345678";
 
-// setup static ip adress
-IPAddress localIP(192, 168, 4, 2);      // target IP adress for the ESP32
-IPAddress gateway(192, 168, 4, 1);      // Router-IP, but it dosen't matter
+long _lastMillis = 0;
+
+#include <Arduino.h>
+#include <WiFi.h>
+// Festgelegte IP-Konfiguration
+IPAddress localIP(192, 168, 4, 2);    // Gewünschte IP des ESP32
+IPAddress gateway(192, 168, 4, 1);      // Router-IP
 IPAddress subnet(255, 255, 255, 0);     // Subnetzmaske
-IPAddress dns(255, 255, 255, 0);        // base setup 
+IPAddress dns(255, 255, 255, 0);              // DNS (z. B. Google DNS)
 
-const char* _serverIP = "192.168.4.1";  // IP des ESP32-Servers
-const uint16_t _serverPort = 8080;         // Port des Servers
-WiFiClient _client;
-
-long _timeoutForReconnect = 30;
-
-// ========================================================================================
-// any
-
-unsigned long _lastMillis = 0;
+int _serverPort = 8080;  
+WiFiServer _server;
 
 
 void setup() {
   
+  // activ signal LED
+  //pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(115200);
 
   Serial.println("FPV Receiver Controller");
 
-  // ----------------------------------------------
   // init camera
   cameraInit();
 
-// ----------------------------------------------
   // setup WLAN
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  //WiFi.config(localIP, gateway, subnet, dns); // Statische IP setzen
-  //WiFi.setAutoReconnect(true);
+  WiFi.config(localIP, gateway, subnet, dns); // Statische IP setzen
+  WiFi.setAutoReconnect(true);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print("WiFi Status: "); Serial.println(WiFi.status());
@@ -77,91 +70,94 @@ void setup() {
 
   Serial.println("WiFi connected! ");
   Serial.print("IP: ");
-  Serial.println(WiFi.localIP()); 
+  Serial.println(WiFi.localIP());
 
-  connectToServer();
-  Serial.println("New Client.");  // print a message out the serial port
+  // start server
+  Serial.print("start server with port number ");
+  char buffer[12];
+  sprintf(buffer, "%d", _serverPort);
+  Serial.println(buffer);
 
-    _lastMillis = millis();
+  _server.begin(_serverPort); 
+
+  _lastMillis = millis();
+
+  Serial.println("Server started! ");
 }
 
 void loop() {
+
   
-  unsigned long actual = millis();
-  if(actual - _lastMillis >= 2000) {
-     _lastMillis = actual;
-    connectToServer();
+  long actual = millis();
+  if(actual < _lastMillis + 2000) {
+     return;
    }
-   
-  if(_client) {
+   _lastMillis = actual;
+
+  Serial.println("------------------------------");
+  Serial.print("IP: "); Serial.println(WiFi.localIP());
+  Serial.println("wait for client connecting");
+  WiFiClient localclient = _server.accept();
+
+
+  if(localclient) {
 
     Serial.println("connection accept");
 
-    if(_client.connected()){
+    while(localclient.connected()){
       Serial.println("connected ");
+
+      //digitalWrite(LED_BUILTIN, true);
 
       camera_fb_t *fb = esp_camera_fb_get();
       if(fb){
         Serial.println("");
       }
-      _client.write(fb->buf, fb->len);
-      Serial.println("Picture has send!");
+      localclient.write(fb->buf, fb->len);
       esp_camera_fb_return(fb);
-
-
-      // Warte auf Bestätigung vom Server
-      unsigned long startTime = millis();
-      bool ackReceived = false;
-      while (millis() - startTime < 1000) {  // Warte max. 1 Sekunde auf ACK
-        if (_client.available()) {
-          String response = _client.readStringUntil('\n');
-          if (response == "ACK") {
-            ackReceived = true;
-            Serial.println("Server has responsed!");
-            break;
-          }
-          Serial.println("Wait for response!");
-        }
-        else {
-          Serial.println("Connection break! No ACK");
-        }
-        delay(10);
-      }
-
+      //digitalWrite(LED_BUILTIN, false);
 
       delay(10);
     }
-    else {
-      delay(1000);
-      //_client.stop();
-      Serial.println("Connection break!");
-    }
 
-    // for(int i = 0; i < 4; i++) {
-    //   delay(1000);
-    //   Serial.print("Countdown: "); Serial.println(i, DEC);
-    // }
+    localclient.stop();
+    Serial.println("Connection break!");
+
+    for(int i = 0; i < 4; i++) {
+      //digitalWrite(LED_BUILTIN, true);
+      delay(500);
+      //digitalWrite(LED_BUILTIN, false);
+      delay(500);
+      Serial.print("Countdown: "); Serial.println(i, DEC);
+    }
     Serial.println();
   }
+  // else {
+  //   Serial.println("try in two second");
+  //   _server.stop();
+  //   delay(2000);
+  //   _server.setTimeout(3);
+  //   _server.begin(_serverPort);
+  // }
 
-  //Serial.println("try to reconnect");  
+  Serial.println("try in two second");  
 }
 
-void connectToServer() {
-  Serial.print("connect to server with port number ");
-  char buffer[12];
-  sprintf(buffer, "%d", _serverPort);
-  Serial.println(buffer);
+// void sendNextPicture() {
 
-if(!_client.connected()) {
-  if(_client.connect(_serverIP, _serverPort)){
-    Serial.print("Connecto to IP "); Serial.println(_serverIP);
-  }
-  else {
-    Serial.println("No connection with a client!");
-  }
-}
+//   // long actual = millis();
+//   // if(actual < _lastMillis + 10) {
+//   //   return;
+//   // }
+//   // _lastMillis = actual;
 
-Serial.print("IP: "); Serial.println(WiFi.localIP());
+//   digitalWrite(LED_BUILTIN, true);
 
-}
+//   camera_fb_t *fb = esp_camera_fb_get();
+//   if(fb){
+//     Serial.println("");
+//   }
+//   _client.write(fb->buf, fb->len);
+
+//   digitalWrite(LED_BUILTIN, false);
+// }
